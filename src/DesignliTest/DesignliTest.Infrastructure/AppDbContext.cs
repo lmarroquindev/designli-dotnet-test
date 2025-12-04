0976ad25c52e9113e0a02e6fab25fd3b69edca24
lmarroquindev <luismarroquindev@gmail.com>
2025-12-04 00:53:31 -0600

    SCRUM-18 - feat(Infrastructure) - Added DbContext implementation to Infrastructure project

---
+++ src/DesignliTest/DesignliTest.Infrastructure/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using DesignliTest.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesignliTest.Infrastructure
+{
+    /// <summary>
+    /// Application database context.
+    /// </summary>
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        /// <summary>
+        /// Employees table.
+        /// </summary>
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
