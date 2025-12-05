c5650e62a4d2120abf765ae312a7cb95e09e5e9c
lmarroquindev <luismarroquindev@gmail.com>
2025-12-04 21:49:29 -0600

    SCRUM-25 - feat(Core.Dto) - Added UserLoginRequest class

---
+++ src/DesignliTest/DesignliTest.Core/Dto/Input/UserApp/UserLoginRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace DesignliTest.Core.Dto.Input.UserApp
+{
+    /// <summary>
+    /// Contains the credentials required to authenticate a user.
+    /// </summary>
+    public class UserLoginRequest
+    {
+        /// <summary>
+        /// Username used to authenticate.
+        /// </summary>
+        public string Username { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Password used to authenticate.
+        /// </summary>
+        public string Password { get; set; } = string.Empty;
+    }
+}
