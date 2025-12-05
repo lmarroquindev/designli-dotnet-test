aaeab13400135a7e63f867f3ab2e7603de826a3d
lmarroquindev <luismarroquindev@gmail.com>
2025-12-05 15:51:14 -0600

    SCRUM-41 - feat(Core) - Added Request helper response Dtos

---
+++ src/DesignliTest/DesignliTest.Core/Dto/Output/Helpers/HttpRequestHelper/RequestResultEntidad.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignliTest.Core.Dto.Output.Helpers.HttpRequestHelper
+{
+    /// <summary>
+    /// Represents the result of an HTTP request that returns a typed entity.
+    /// </summary>
+    /// <typeparam name="TEntidad">Type of the deserialized entity.</typeparam>
+    public class RequestResultEntidad<TEntidad> : RequestResult
+    {
+        /// <summary>
+        /// Deserialized entity returned by the API.
+        /// </summary>
+        public TEntidad Entidad { get; set; }
+    }
+}
