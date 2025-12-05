734d19502afdeb28656c873afa27f14dd6d80b47
lmarroquindev <luismarroquindev@gmail.com>
2025-12-05 15:52:05 -0600

    SCRUM-41 - feat(Web) - Added AuthClientService interface and implementation class with methods to request JWT to API

---
+++ src/DesignliTest/DesignliTest.Web/Interface/IAuthClientService.cs
@@ -0,0 +1,18 @@
+﻿using DesignliTest.Core.Dto.Input.UserApp;
+
+namespace DesignliTest.Web.Interface
+{
+    /// <summary>
+    /// Definition of methods to authenticate users.
+    /// </summary>
+    public interface IAuthClientService
+    {
+        /// <summary>
+        /// Sends user credentials to the API authentication endpoint.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>Returns the JWT token if login is successful; otherwise null.</returns>
+        Task<string?> LoginAsync(UserLoginRequest model);
+
+    }
+}
