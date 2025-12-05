26c6ec7f0386447a987f67d2736bff7e6d83f46b
lmarroquindev <luismarroquindev@gmail.com>
2025-12-05 16:09:58 -0600

    SCRUM-41 - feat(Web) - Added extension to configure dependency injections

---
+++ src/DesignliTest/DesignliTest.Web/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using DesignliTest.Application.Helpers;
+using DesignliTest.Core.Interface.Helpers;
+using DesignliTest.Web.Interface;
+using DesignliTest.Web.Services;
+
+namespace DesignliTest.Web.Extensions
+{
+    /// <summary>
+    /// Extensions to register services and helpers for the Web project.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Registers the necessary services for the web application (Razor Pages, HttpClient, and internal services).
+        /// </summary>
+        public static IServiceCollection AddWebServices(this IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddHttpClient();
+
+            // Helpers
+            services.AddScoped<IHttpRequestHelper, HttpRequestHelper>();
+
+            // Internal Services
+            services.AddScoped<IAuthClientService, AuthClientService>();
+            services.AddScoped<IUserClientService, UserClientService>();
+
+            services.AddHttpContextAccessor();
+
+            return services;
+        }
+    }
+}
