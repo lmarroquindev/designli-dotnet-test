aef4c12ecd0632a9af7008ea31ce1289a6543ec1
lmarroquindev <luismarroquindev@gmail.com>
2025-12-05 16:10:39 -0600

    SCRUM-41 - feat(Web) - Added dependency i jections configuration to program class

---
+++ src/DesignliTest/DesignliTest.Web/Program.cs
@@ -0,0 +1,34 @@
+using DesignliTest.Web.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddWebServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.MapGet("/", context =>
+{
+    context.Response.Redirect("/Login");
+    return Task.CompletedTask;
+});
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseJwtAuth();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
